a02d3692b6ab03a457e01e404d10de9269aa47e7
julianamilhomens <julianamilhomensdesiqueira@gmail.com>
2026-02-03 22:41:21 -0300

    fix: vercel rewrite for react router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,31 +1,45 @@
 import { Route, Routes } from "react-router-dom";
 
-import { Cart, Checkout, CompletePayment, EditProduct, Home, Login, Menu, NewProduct, Orders, Products, Register, Trash } from '../containers';
+import {
+  Cart,
+  Checkout,
+  CompletePayment,
+  EditProduct,
+  Home,
+  Login,
+  Menu,
+  NewProduct,
+  Orders,
+  Products,
+  Register,
+  Trash
+} from '../containers';
+
 import { UserLayout } from '../layouts/UserLayout';
 import { AdminLayout } from "../layouts/AdminLayout";
 
 export function Router() {
-    return (
-        <Routes>
-            <Route path="/" element={<UserLayout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/cardapio" element={<Menu />} />
-                <Route path="/carrinho" element={<Cart />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/complete" element={<CompletePayment />} />
-                
-            </Route>
+  return (
+    <Routes>
+      <Route path="/" element={<UserLayout />}>
+        <Route index element={<Home />} />
+        <Route path="cardapio" element={<Menu />} />
+        <Route path="carrinho" element={<Cart />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="complete" element={<CompletePayment />} />
+      </Route>
 
-            <Route path="/admin" element={<AdminLayout />}>
-                <Route path="/admin/pedidos" element={<Orders />} />
-                <Route path="/admin/novo-produto" element={<NewProduct />} />
-                <Route path="/admin/editar-produto" element={<EditProduct />} />
-                <Route path="/admin/produtos" element={<Products />} />
-                <Route path="/admin/lixeira" element={<Trash />} />
-            </Route>
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<Orders />} />
+        <Route path="pedidos" element={<Orders />} />
+        <Route path="novo-produto" element={<NewProduct />} />
+        <Route path="editar-produto" element={<EditProduct />} />
+        <Route path="produtos" element={<Products />} />
+        <Route path="lixeira" element={<Trash />} />
+      </Route>
 
-            <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<Register />} />
-        </Routes >
-    );
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Register />} />
+    </Routes>
+  );
 }
